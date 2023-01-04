0269ec0948db73bf8b685390e4a3fa2b6115a649
dvanca <vanca37@gmail.com>
2023-01-04 14:27:15 +0200

    fixed some files

---
+++ src/main/app/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  useEffect(() => {
+    axios.get('/appointments').
+      then((response) => console.log(response.data));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
